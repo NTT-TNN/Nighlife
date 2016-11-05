7bee43b6e61135746ca39d0d571151181186cee9
thao nguyentien <thao nguyentien>
2016-11-05 12:22:54 +0700

    fix router twitter

--- routes/users.js
+++ routes/users.js
@@ -49,8 +49,8 @@ router.get('/auth/facebook/callback', passport.authenticate('facebook', {
 router.get('/auth/twitter', passport.authenticate('twitter'));
 
 router.get('/auth/twitter/callback', passport.authenticate('twitter', {
-  successRedirect: '/users/profile',
-  failureRedirect: '/home',
+  successRedirect: '/users',
+  failureRedirect: '/users/login',
 }));
 
 
